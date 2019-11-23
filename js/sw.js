9f2b0c88eba7ed380438bb3e89e901859b0557b4
Lucaspitas <56032011+Lucaspitas@users.noreply.github.com>
2019-11-22 21:58:05 -0300

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,56 +1,26 @@
----
-layout: null
----
-
-const staticCacheName = 'lucas-pitas-{{ site.time | date: "%Y-%m-%d-%H-%M" }}';
-
-const filesToCache = [
-  { % for page in site.pages_to_cache % }
-    '{ { page } }',
-  { % endfor % }
-  { % for post in site.posts limit: 6 % }
-    '{ { post.url } }',
-  { % endfor % }
-  { % for asset in site.files_to_cache % }
-    '{ { asset } }',
-  { % endfor % }
-];
-
-// Cache on install
-this.addEventListener("install", event => {
-  this.skipWaiting();
-
-  event.waitUntil(
-    caches.open(staticCacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-    })
-  )
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
+navigator.serviceWorker.register('/service-worker.js', {
+  scope: '/app/'
 });
-
-// Clear cache on activate
-this.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => (cacheName.startsWith('willian-justen-')))
-          .filter(cacheName => (cacheName !== staticCacheName))
-          .map(cacheName => caches.delete(cacheName))
-      );
-    })
-  );
+navigator.serviceWorker.register('/app/service-worker.js', {
+  scope: '/app'
 });
-
-// Serve from Cache
-this.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(() => {
-        return caches.match('/offline/index.html');
-      })
-  )
+location ~* (service-worker\.js)$ {
+  # tells browsers the service worker scope
+  add_header 'Service-Worker-Allowed' '/app';
+}
+// Listen for install event, set callback
+self.addEventListener('install', function(event) {
+    // Perform some task
+});
+self.addEventListener('activate', function(event) {
+  // Perform some task
 });
